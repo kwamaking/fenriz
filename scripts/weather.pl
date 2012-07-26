ef6fa479bec9c512710609c55c361163bfca9584
kwamaking <kwamaking@gmail.com>
2012-07-26 13:41:17 -0500

    Tidied up weather script

--- scripts/weather.pl
+++ scripts/weather.pl
@@ -16,21 +16,22 @@ sub get_weather {
 	$ua->timeout(10);
 	my $url = 'http://38.102.136.104/auto/raw/'.$cmd[1];
 	my $results = $ua->get($url);
-	my @badarray = split(/[|]\s*/, $results->content);
+	my @weather = split(/[|]\s*/, $results->content);
 	if (!$results->is_success) {
 		$str = "Sorry, no weather available for your location, For help and formatting use: ~weather help";
 	}
-	elsif (!$badarray[0]) {
+	elsif (!$weather[0]) {
 		$str = "Sorry, no weather available for your location, For help and formatting use: ~weather help";
 	}
 	else {
-		my @goodarray = split(/[|]\s*/, $results->content);
-		if ($goodarray[1] eq "") { 
-			$str = "\002Conditions for $goodarray[18], $goodarray[19] at $goodarray[0]:\002 $goodarray[8] \002Temp:\002 $goodarray[1] F \002Humidity:\002 $goodarray[4] \002Barometer:\002 $goodarray[7] \002Wind:\002 $goodarray[6] mph";
+		if ($weather[1] eq "") { 
+			$str = "\002Conditions for $weather[18], $weather[19] at $weather[0]:\002 $weather[8] \002Temp:\002 $weather[1] F ";
+			$str .= "\002Humidity:\002 $weather[4] \002Barometer:\002 $weather[7] \002Wind:\002 $weather[6] mph";
 		}
 		else {
-			my $ctemp = sprintf( "%4.1f", ($goodarray[1] - 32) * (5 / 9) );
-			$str = "\002Conditions for $goodarray[18], $goodarray[19] at $goodarray[0]:\002 $goodarray[8] \002Temp:\002 $goodarray[1] F/$ctemp C \002Humidity:\002 $goodarray[4] \002Barometer:\002 $goodarray[7] \002Wind:\002 $goodarray[6] mph";
+			my $ctemp = sprintf( "%4.1f", ($weather[1] - 32) * (5 / 9) );
+			$str = "\002Conditions for $weather[18], $weather[19] at $weather[0]:\002 $weather[8] \002Temp:\002 $weather[1] F/$ctemp C ";
+			$str .= "\002Humidity:\002 $weather[4] \002Barometer:\002 $weather[7] \002Wind:\002 $weather[6] mph";
 		}
 
 	}
