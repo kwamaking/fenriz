508a45bfee34260541ce7d03b4bb5001713ebf6d
kwamaking <kwamaking@gmail.com>
2012-07-26 13:24:07 -0500

    Updating help information for new commands

--- scripts/help.pl
+++ scripts/help.pl
@@ -3,22 +3,26 @@ sub help {
 	my ($server, $target, $nick, @cmd) = @_;
 	my $commandtype = 'notice';
 	my @help = (
+		'--List of available commands::Brackets indicate optional parameter--',
 		'--Last FM Commands:',
 		'~np [username]       - shows your currently playing song, or of another user if specified',
 		'~compare u1 [u2]     - compares yourself with u1 (another user) if u2 isn\'t specified',
-		'~setuser [user]      - associates the "user" last.fm username with your nickname.',
-		'~top or ~top [user]  - Shows recent top artists of the user',
-		'~whois [username]    - given a last.fm username, return all nicknames that are associated to it.',
+		'~setuser user        - associates the "user" last.fm username with your nickname.',
+		'~top [user]  - Shows recent top artists of the user',
+		'~whois username      - given a last.fm username, return all nicknames that are associated to it.',
 		'~deluser             - removes your last.fm association.',
-		'~band [artist name]  - displays information about given band or artist',
-		'~plays [artist name] - displays playcount of given artist',
+		'~band artist         - displays information about given band or artist',
+		'~plays artist        - displays playcount of given artist',
 		'--Weather Commands:',
-		'~weather [zip]       - you will receive a notice with current weather conditions use it like this: ~weather zipcode or ~weather city,country if the city has a space in it, use an underscore.  i.e., ~weather San_Diego,California',
-		'@weather [zip]       - this broadcasts weather conditions into the channel',
+		'~weather zip         - you will receive a notice with current weather conditions.',
+		'@weather zip         - this broadcasts weather conditions into the channel',
+		'~weather help        - this displays some helpful hints for getting the weather',
 		'--Urban Dictionary Commands:',
-		'~ud [term]           - returns a few lines from urban dictionary with specified term',
+		'~ud term             - returns a few lines from urban dictionary with specified term',
+		'--Wikipedia:',
+		'~wiki term           - returns a few lines wikipedia with specified term',
 		'--Down for everyone or just me?:',
-		'~checksite [website]      - Checks if a website is up or down',
+		'~checksite website   - Checks if a website is up or down',
 	);
 	for (@help) { #sends each element in array to window until it's empty
 		send_msg_alt($server, $nick, $_, $commandtype);
